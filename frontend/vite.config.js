6efddedf9066337324a0e31c3db99f1ae928c9b7
shreelakshmibharadwaj <bharadwajshreelakshmi@gmail.com>
2024-10-10 15:40:06 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default {
-  build: {
-    outDir: 'dist', // this is the default
-  }
-}
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   build: {
+    outDir: 'dist', 
   plugins: [react()],
   server: {port:5173}
 })
